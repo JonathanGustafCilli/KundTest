76f6fd78c97d51094bd68918dc44456af9badc0b
Jon II <jonathan.gustaf.cilli@gmail.com>
2016-12-17 20:29:29 +0100

    It was me
    I connected the html with the css

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
-<head>
-</head>
-<body>
-	<p> TESTING THIS FUCKING REVOLTING THING </p>
-	<h1> SO, THIS LOOKS GOOD </h1>
-</body>
+	<head>
+		<link href="stylesheets/style.css" rel='stylesheet' type='text/css'>	
+	</head>
+	<body>
+		<p> TESTING THIS FUCKING REVOLTING THING </p>
+		<h1> SO, THIS LOOKS GOOD </h1>
+	</body>
 </html>
